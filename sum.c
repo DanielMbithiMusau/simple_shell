00062de46ba74041c033a0875d6b09a944aabe9c
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-05-11 07:52:57 -0700

    Betty code that passes all checks

---
+++ sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Adds two numbers and bring out result.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a, b, sum;
+
+	a = 5;
+	b = 10;
+
+	sum = a + b;
+
+	printf("Sum of a and b is: %d\n", sum);
+
+	return (0);
+}
